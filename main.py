c078361a5439b27bb02c780db28b47e44b21f9ec
JonesITHS <jonas.johnsson@iths.se>
2024-08-23 10:57:43 +0200

    Restore '8/23/2024, 10:28:17 AM Initialize empty repo'

--- main.py
+++
@@ -1,8 +0,0 @@
-mySprite = sprites.create(assets.image("""
-    Dragonfly
-"""), SpriteKind.player)
-controller.move_sprite(mySprite)
-
-def on_on_update():
-    pass
-game.on_update(on_on_update)
--- main.ts
+++
@@ -1,7 +0,0 @@
-let p1 = mp.playerSelector(mp.PlayerNumber.One)
-let p2 = mp.playerSelector(mp.PlayerNumber.Two)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
-game.onUpdate(function () {
-	
-})
