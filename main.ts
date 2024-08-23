c343f191abd5ff989634065bf6107ca4a7ab5899
JonesITHS <jonas.johnsson@iths.se>
2024-08-23 10:34:02 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-let mySprite = sprites.create(assets.image`Dragonfly`, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
 game.onUpdate(function () {
+    let mySprite: Sprite = null
     controller.moveSprite(mySprite)
-    scene.cameraFollowSprite(mySprite)
 })
