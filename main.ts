3a9dc014e668a808fc0deaed6fbdda8c9262fa0d
YongWeiJ <martina.malmberg@iths.se>
2024-08-23 10:51:55 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@ let p2 = mp.playerSelector(mp.PlayerNumber.Two)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
 game.onUpdate(function () {
-	
+    game.reset()
 })
