ec8d9127a0b571ae3225849ed5a19a457cb53c83
JonesITHS <jonas.johnsson@iths.se>
2024-08-23 10:46:16 +0200

    P1 and p2

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+let p1 = mp.playerSelector(mp.PlayerNumber.One)
+let p2 = mp.playerSelector(mp.PlayerNumber.Two)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Dragonfly`, SpriteKind.Player))
 game.onUpdate(function () {
-    let mySprite: Sprite = null
-    controller.moveSprite(mySprite)
+	
 })
